340d3d59638eaf6c264cd8220264f8c402f85f9c
Wellington Araújo <wellingtonlbaraujo13@gmail.com>
2025-09-02 11:28:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const weddingDate = new Date('2026-04-28T00:00:00');
+
+function updateCountdown() {
+    const now = new Date();
+    const difference = weddingDate - now;
+    
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+    document.getElementById('days').textContent = days.toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
+
+let slideIndex = 0;
+mostrarSlides();
+
+function mostrarSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex-1].style.display = "block";
+
+  setTimeout(mostrarSlides, 5000);
+}
